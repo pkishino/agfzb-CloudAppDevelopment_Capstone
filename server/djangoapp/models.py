a7a003788f371392756c3773e8bc8a053819d08f
Patrick Kishino <patrick.a.kishino@gmail.com>
2021-12-15 04:08:08 +0000

    root user created and carmake/carmodels models done

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 from django.utils.timezone import now
 
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='car maker')
+    description = models.CharField(max_length=1000)
+    def __str__(self):
+        return "Name:"+self.name+"\nDescription:"+self.description
 
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    maker = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30, default='car model')
+    description = models.CharField(max_length=1000)
+    model_year = models.DateField(null=True)
+    dealer_id = models.IntegerField(default=1)
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    SEDAN = 'Sedan'
+    CONVERTIBLE = 'Convertible'
+    model_types = [
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (SEDAN, 'Sedan'),
+        (CONVERTIBLE, 'Convertible')
+    ]
+    model_type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=model_types,
+        default=SEDAN
+    )
+    def __str__(self):
+        return "Maker:"+self.maker.name+"\nmodel:"+self.name+" Year:"+self.model_year+\
+            "\nDetails:\n   -"+self.description+"\n   -"+self.model_type+"\n Available at:"+self.dealer_id+"(Dealer id)"
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
